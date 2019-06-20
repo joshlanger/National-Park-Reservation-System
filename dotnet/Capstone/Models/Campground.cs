a53c2aa35c2dc2d5bc47e532e3301e3f133f0166
Gus Miller <angusmiller111@gmail.com>
2019-06-20 15:31:27 -0400

    revised class data

---
+++ dotnet/Capstone/Models/Campground.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class Campground
+    {
+        public int CampgroundId { get; set; }
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public int OpenFrom { get; set; }
+        public int OpenTo { get; set; }
+        public decimal Fee { get; set; } 
+    }
+}
