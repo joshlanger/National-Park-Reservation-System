55c373de9e844e60995c57268ec03aaf18415875
Josh Langer <joshlanger@gmail.com>
2019-06-20 14:05:20 -0400

    working on skeleton

---
+++ dotnet/Capstone/DAL/ParkSqlDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.DAL;
+using Capstone.Models;
+using System.IO;
+
+namespace Capstone.DAL
+{
+    public class ParkSqlDAO
+    {
+       
+    }
+}
---
+++ dotnet/Capstone/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Capstone
+{
+    public class Menu
+    {
+        public void Intro()
+        {
+            Console.WriteLine("Welcome to the National Park Service reservation system.");
+            
+        }
+        public void ViewParks()
+        {
+            Console.WriteLine("Select a Park for Further Details");
+            Console.WriteLine("1) Acadia");
+            Console.WriteLine("2) Arches");
+            Console.WriteLine("3) Cuyahoga National Valley Park");
+            Console.WriteLine("Q) Quit");
+            Console.ReadLine();
+        }
+    }
+}
