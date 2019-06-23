2f32dd1266bd45696a576bd44aa779c8d673cd44
Gus Miller <angusmiller111@gmail.com>
2019-06-23 17:01:56 -0400

    Updates

--- dotnet/Capstone.Tests/SiteTests.cs
+++ dotnet/Capstone.Tests/SiteTests.cs
@@ -1,5 +1,4 @@
 ﻿using Capstone.DAL;
-using Capstone.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -9,23 +8,11 @@ namespace Capstone.Tests
 {
     [TestClass]
 
-    public class SiteTests : TestsBaseClass
+    class SiteTests
     {
         private SiteSqlDAO dao { get; set; }
 
-    }
-        //[TestMethod]
 
-        //public void ChooseSite()
-        //{
-        //    dao = new SiteSqlDAO(ConnectionString);
-        //    IList<SiteSqlDAO> getAllSites = dao.ReservationTime.getAllSites()
-        //}
-        //{
-        //    dao = new SiteSqlDAO(ConnectionString);
-        //    IList<Site> getReservation = dao.ReservationTime(...);
-        //    Assert.AreEqual(2, getReservation);
-        //}
-    
-    
+
+    }
 }
